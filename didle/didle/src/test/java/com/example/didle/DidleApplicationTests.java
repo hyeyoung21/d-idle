6cd471818268a96973b1335a76d5f55af23e9e9f
rhg4656 <llloe@naver.com>
2025-03-19 12:20:33 +0900

    3/19 테스트5

--- didle/didle/src/test/java/com/example/didle/DidleApplicationTests.java
+++ didle/didle/src/test/java/com/example/didle/DidleApplicationTests.java
@@ -1,15 +1,22 @@
 package com.example.didle;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
 class DidleApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("🔍 Environment Variables:");
+        System.out.println(" - SPRING_DATASOURCE_URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+        System.out.println(" - SPRING_DATASOURCE_USERNAME: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+        System.out.println(" - SPRING_DATASOURCE_PASSWORD: " + System.getenv("SPRING_DATASOURCE_PASSWORD"));
+
+        System.out.println("\n🔍 Application Properties:");
+        System.out.println(" - spring.datasource.url: " + System.getProperty("spring.datasource.url"));
+        System.out.println(" - spring.datasource.username: " + System.getProperty("spring.datasource.username"));
+        System.out.println(" - spring.datasource.password: " + System.getProperty("spring.datasource.password"));
     }
 
 }
