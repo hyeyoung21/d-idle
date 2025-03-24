bc19686b3f3618243fcf00a53acb552f22a60a10
rhg4656 <llloe@naver.com>
2025-03-24 11:20:29 +0900

    3/24 1120 s3 테스트

--- didle/didle/src/test/java/com/example/didle/DidleApplicationTests.java
+++ didle/didle/src/test/java/com/example/didle/DidleApplicationTests.java
@@ -3,6 +3,13 @@ package com.example.didle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties = {
+        "spring.cloud.aws.credentials.access-key=test-access-key",
+        "spring.cloud.aws.credentials.secret-key=test-secret-key",
+        "spring.cloud.aws.region.static=us-east-2"
+})
 
 @SpringBootTest
 @ActiveProfiles("test")
