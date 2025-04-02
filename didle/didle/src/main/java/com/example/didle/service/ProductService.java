3cc95d8f0237e4d95540fcd89cb01d531e0b023b
rhg4656 <llloe@naver.com>
2025-04-02 17:12:42 +0900

    웹개발 css 수정2

--- didle/didle/src/main/java/com/example/didle/service/ProductService.java
+++ didle/didle/src/main/java/com/example/didle/service/ProductService.java
@@ -10,16 +10,21 @@ import com.example.didle.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,26 +35,26 @@ public class ProductService {
     private final OrderItemRepository orderItemRepository;
     private final CartItemRepository cartItemRepository;
     private final CategoryRepository categoryRepository;
-//    private final S3Client s3Client;
-//    private final String bucketName;
-//    private final String region;
+    private final S3Client s3Client;
+    private final String bucketName;
+    private final String region;
 
     public ProductService(ProductRepository productRepository,
                           OrderItemRepository orderItemRepository,
                           CartItemRepository cartItemRepository,
                           CategoryRepository categoryRepository
-//            ,
-//                          S3Client s3Client,
-//                          @Value("${spring.cloud.aws.s3.bucket}") String bucketName,
-//                          @Value("${spring.cloud.aws.region.static}") String region
+            ,
+                          S3Client s3Client,
+                          @Value("${spring.cloud.aws.s3.bucket}") String bucketName,
+                          @Value("${spring.cloud.aws.region.static}") String region
     ) {
         this.productRepository = productRepository;
         this.orderItemRepository = orderItemRepository;
         this.cartItemRepository = cartItemRepository;
         this.categoryRepository = categoryRepository;
-//        this.s3Client = s3Client;
-//        this.bucketName = bucketName;
-//        this.region = region;
+        this.s3Client = s3Client;
+        this.bucketName = bucketName;
+        this.region = region;
     }
 
     public Product createProduct(Product product) {
@@ -135,32 +140,32 @@ public class ProductService {
             product.setCategory(category);
         }
 
-//        // 이미지 처리
-//        if (image != null && !image.isEmpty()) {
-//            // 1. 고유한 파일 이름 생성
-//            String fileName = UUID.randomUUID().toString() + "_" + image.getOriginalFilename();
-//
-//            System.out.println("++++++++++++++++++++++++++++++++++++++++++" + s3Client.toString());
-//            System.out.println("++++++++++++++++++++++++++++++++++++++++++" + bucketName);
-//
-//
-//            // 2. S3에 파일 업로드
-//            s3Client.putObject(
-//                    PutObjectRequest.builder()
-//                            .bucket(bucketName)
-//                            .key(fileName)
-//                            .build(),
-//                    RequestBody.fromInputStream(image.getInputStream(), image.getSize())
-//            );
-//
-//            System.out.println("++++++++++++++++++++++++++++++++++++++++++" + s3Client.toString());
-//
-//            // 3. 업로드된 파일의 URL 생성
-//            String imageUrl = String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
-//
-//            // 4. Product 엔티티에 이미지 URL 설정
-//            product.setImageUrl(imageUrl);
-//        }
+        // 이미지 처리
+        if (image != null && !image.isEmpty()) {
+            // 1. 고유한 파일 이름 생성
+            String fileName = UUID.randomUUID().toString() + "_" + image.getOriginalFilename();
+
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++" + s3Client.toString());
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++" + bucketName);
+
+
+            // 2. S3에 파일 업로드
+            s3Client.putObject(
+                    PutObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key(fileName)
+                            .build(),
+                    RequestBody.fromInputStream(image.getInputStream(), image.getSize())
+            );
+
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++" + s3Client.toString());
+
+            // 3. 업로드된 파일의 URL 생성
+            String imageUrl = String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
+
+            // 4. Product 엔티티에 이미지 URL 설정
+            product.setImageUrl(imageUrl);
+        }
 
 
         Product savedProduct = productRepository.save(product);
